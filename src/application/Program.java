345d81678979262299459546f939cd3be15dc083
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2019-11-10 17:02:27 -0300

    BoardException and Defensive Programming

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,6 @@ import boardgame.Position;
 import chess.ChessMatch;
 
 public class Program {
-
 	public static void main(String[] args) {
 		ChessMatch chessmatch = new ChessMatch();
 		UI.printBoard(chessmatch.getPieces());
